0300321e24fd771d1c7750b3ba7b7c1000a6ecd1
Subha8436 <124521782+Subha8436@users.noreply.github.com>
2024-07-27 22:41:32 +0530

    Update weather.js

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,11 +1,20 @@
 const axios = require('axios');
 
-exports.handler = async (event, context) => {
-  const apiKey = process.env.WEATHER_API_KEY;
-  const city = event.queryStringParameters.city;
+exports.handler = async (event) => {
+  const apiKey = process.env.0abedba42881be4104bab90a6388420e;
+  const city = event.queryStringParameters.city || '';
+  const lat = event.queryStringParameters.lat || '';
+  const lon = event.queryStringParameters.lon || '';
 
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    let url = '';
+    if (city) {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    } else if (lat && lon) {
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    }
+
+    const response = await axios.get(url);
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
